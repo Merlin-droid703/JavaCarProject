b8297865251cbc7487d21f211ca087dd9f17b668
Merlin-droid703 <melisa_aladag@hotmail.com>
2025-06-01 16:38:08 +0300

    Polymorphism örneği eklendi: Animal, Cat, Dog sınıfları ve TestPolymorphism oluşturuldu

---
+++ src/Dog.java
@@ -0,0 +1,7 @@
+// Dog.java
+public class Dog extends Animal {
+    @Override
+    public void makeSound() {
+        System.out.println("Köpek havlar: Hav hav!");
+    }
+}
---
+++ src/TestPolymorphism.java
@@ -0,0 +1,27 @@
+// TestPolymorphism.java
+public class TestPolymorphism {
+    public static void main(String[] args) {
+        // Animal türünde bir referans, Cat nesnesini işaret ediyor.
+        Animal animal1 = new Cat();
+
+        // Animal türünde bir referans, Dog nesnesini işaret ediyor.
+        Animal animal2 = new Dog();
+
+        System.out.println("animal1 (bir Kedi) ses çıkarıyor:");
+        animal1.makeSound(); // Cat sınıfındaki makeSound() çağrılacak
+
+        System.out.println("\nanimal2 (bir Köpek) ses çıkarıyor:");
+        animal2.makeSound(); // Dog sınıfındaki makeSound() çağrılacak
+
+        System.out.println("\n--- Ekstra Örnek: Dizide Kullanım ---");
+        Animal[] hayvanlar = new Animal[3];
+        hayvanlar[0] = new Dog();
+        hayvanlar[1] = new Cat();
+        hayvanlar[2] = new Animal(); // Genel Animal nesnesi
+
+        for (Animal hayvan : hayvanlar) {
+            System.out.print("Dizideki hayvan: ");
+            hayvan.makeSound();
+        }
+    }
+}
